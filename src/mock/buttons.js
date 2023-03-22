41061bf919681d5ba5505ccd46d92246e0e8ee2d
noah <qny123@naver.com>
2023-03-22 17:51:49 +0900

    chore: 불필요한 파일 제거

--- src/mock/buttons.js
+++
@@ -1,7 +0,0 @@
-const buttonArr = [
-  { title: "Hotels", icon: "hotel" },
-  { title: "Ticket", icon: "ticket" },
-  { title: "Airplane", icon: "plane" },
-  { title: "Train", icon: "train" },
-];
-export { buttonArr };
